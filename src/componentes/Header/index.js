7c3da653be0ee1f2a66b9b51af470c4ac39486a2
Digowmarins <heytorsouza9@gmail.com>
2023-01-04 22:18:29 -0300

    Alura-Books
    Feito durante o curso de React da alura

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,24 @@
+
+import Logo from '../logo'
+import OpcoesHeader from '../OpcoesHeader'
+import IconeHeader from '../IconesHeader'
+import styled from 'styled-components'
+
+const HeaderContainer = styled.header`
+        background-color: #fff;
+        display: flex;
+        justify-content: center;
+`
+
+
+function Header() {
+    return (
+        <HeaderContainer>
+            <Logo/>
+            <OpcoesHeader/>
+            <IconeHeader/>
+        </HeaderContainer>
+    )
+}
+
+export default Header
